563f7ff1ae6863af1754ac3ca706129bcdb53e40
The-Ghost56 <131254680+The-Ghost56@users.noreply.github.com>
2025-03-07 20:47:05 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  let cardNumber = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K"];
+  let suit = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let ramdomCardSuit = Math.floor(Math.random() * suit.length)
+
+document.getElementById('cardcontent').innerHTML = cardNumber[randomCardNumber];
+document.getElementById('card').className = "";
+document.getElementById('card').classList.add("card");
+document.getElementById('card').classList.add(getSuitClass(finalSuit));
+
+}
+
+function getSuitClass(suit) {
+  switch (suit) {
+    case "Hearts": return "suit-hearts";
+    case "Diamonds": return "suit-diamonds";
+    case "Spades": return "suit-spades";
+    case "Clubs": return "suit-clubs";
+
+  }
+}
